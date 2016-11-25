1c72dbf16d77e109b3a764e6fff831fb6effd8f9
haikuoliu <haikuo.liu.cu@gmail.com>
2016-11-25 03:00:26 -0500

    Add ins

--- TwitterMap/TwitterMap/application.py
+++ TwitterMap/TwitterMap/application.py
@@ -1,5 +1,4 @@
-from flask import render_template
-from flask import Flask
+from flask import render_template, request, Flask
 import requests
 import json
 application = Flask(__name__)
@@ -47,9 +46,39 @@ def updatedata(keyword):
     return js_results;
 
 
+@application.route('/sns', methods = ['GET', 'POST', 'PUT'])
+def sns():
+    # AWS sends JSON with text/plain mimetype
+    try:
+        js = json.loads(request.data)
+    except:
+        pass
+
+    hdr = request.headers.get('X-Amz-Sns-Message-Type')
+    # subscribe to the SNS topic
+    if hdr == 'SubscriptionConfirmation' and 'SubscribeURL' in js:
+        requests.get(js['SubscribeURL'])
+
+    if hdr == 'Notification':
+        msg_process(js['Message'], js['Timestamp'])
+
+    return 'OK\n'
+
+
+def msg_process(msg, tstamp):
+    js = json.loads(msg)
+    msg = 'Region: {0} / Alarm: {1}'.format(
+        js['Region'], js['AlarmName']
+    )
+
+
 @application.route('/googlemap/<keyword>')
 def google_map(keyword):
     return render_template('googlemap_markers.html', keyword=keyword)
 
 if __name__ == '__main__':
-    application.run()
+    application.run(
+        host="0.0.0.0",
+        port=5000
+    )
+

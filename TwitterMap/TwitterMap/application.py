e100683809f1a1b5bc9e496ce6ac5dd4710c1dd7
haikuoliu <haikuo.liu.cu@gmail.com>
2016-11-27 03:34:53 -0500

    x

--- TwitterMap/TwitterMap/application.py
+++ TwitterMap/TwitterMap/application.py
@@ -119,8 +119,8 @@ if __name__ == '__main__':
     socketio.run(
         application,
         host="0.0.0.0",
-        port=5000,
-        threaded=True
+        port=5000
+        # threaded=True
     )
 
 

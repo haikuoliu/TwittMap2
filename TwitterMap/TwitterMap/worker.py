d05b6b73a15c3c91416cf23dd830a3ac6e8e3e05
haikuoliu <haikuo.liu.cu@gmail.com>
2016-12-08 21:08:41 -0500

    x

--- TwitterMap/TwitterMap/worker.py
+++ TwitterMap/TwitterMap/worker.py
@@ -16,7 +16,7 @@ sqs = boto3.resource('sqs')
 queue = sqs.get_queue_by_name(QueueName='Erinyes')
 sns = boto3.resource('sns')
 topic = sns.create_topic(Name='SentimentTwitterMap_Test')
-topic.subscribe(Protocol='http', Endpoint='http://54.224.56.204:5000/sns')
+topic.subscribe(Protocol='http', Endpoint='http://54.173.206.140:5000/sns')
 
 alchemy_language = AlchemyLanguageV1(api_key='6de8f373d9e238ea73e7c2d737cc0ab4effb0079')
 url = 'https://gateway-a.watsonplatform.net/calls'
